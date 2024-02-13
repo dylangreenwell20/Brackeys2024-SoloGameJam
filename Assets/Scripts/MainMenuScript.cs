2f070938208367594bc98f4b486a4a7f7fce712a
dylangreenwell20 <dylan.greenwell@students.plymouth.ac.uk>
2024-02-13 02:52:19 +0000

    V 0.17
    Set up "Used Knight" prefab using knight asset.
    
    Clicking "Play" now loads next scene.

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenu; //reference to main menu
+    [SerializeField] private GameObject optionsMenu; //reference to options menu
+
+    private void Start()
+    {
+        mainMenu.SetActive(true); //show main menu
+        optionsMenu.SetActive(false); //hide options menu
+    }
+
     public void PlayGame()
     {
-
+        Debug.Log("Game played..."); //for testing
+        SceneManager.LoadScene(1); //load game scene
     }
 
     public void ExitGame()
